59f57fd47cfd08edeadfd60ffe39f962f11a6353
Ayush Tiwari <studentayush6@gmail.com>
2025-07-30 21:41:12 +0530

    Add files via upload

---
+++ Java-Codes/basics/javaProjectsCP/DuplicatesInArrayCP.java
@@ -0,0 +1,21 @@
+/*Given an integer array nums of length n where all the
+ integers of nums are in the range [1, n] and each integer appears 
+ at most twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) 
+time and uses only constant auxiliary space, excluding the space needed to store the output*/
+
+
+import java.util.ArrayList;
+public class DuplicatesInArrayCP{
+    public static void main(String[] args) {
+        
+    }
+    
+    static List<Integer> findDuplicate (int[] nums){
+        
+
+    }
+
+
+}
