dcf8081bb4b8d17fcb1daf1246bde085cdcb08be
Ayush Tiwari <studentayush6@gmail.com>
2025-08-08 22:41:28 +0530

    Add files via upload

---
+++ Competitive-Programming/Leetcode/Array/missingNumber.java
@@ -0,0 +1,16 @@
+public class missingNumber {
+    public static void main(String[] args) {
+        
+    }
+     public int missingNumber(int[] nums) {
+        int[] arr = new int[nums.length +1]; // to store element from 0 to n.
+        for(int x : nums){
+            arr[x]++; // array indices shows the num while element shows the count 
+            // one of the best methord  to solve freq related ques 
+        }
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i] == 0) return i; // indices gives the number and zero shows it is missing.
+        }
+        return -1; // dummy return 
+    }
+}
