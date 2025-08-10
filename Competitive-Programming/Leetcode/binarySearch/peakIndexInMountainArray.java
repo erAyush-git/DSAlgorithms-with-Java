b9b095d84d7c48eeeb7f252edda810b38dd789dc
Ayush Tiwari <studentayush6@gmail.com>
2025-08-10 19:37:56 +0530

    Update peakIndexInMountainArray.java

--- Competitive-Programming/Leetcode/binarySearch/peakIndexInMountainArray.java
+++ Competitive-Programming/Leetcode/binarySearch/peakIndexInMountainArray.java
@@ -1,3 +1,13 @@
+/*You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+Return the index of the peak element.
+
+Your task is to solve it in O(log(n)) time complexity.
+
+leetcode 852*/
+
+
+
 public class peakIndexInMountainArray {
    public static void main(String[] args) {
     
@@ -18,3 +28,4 @@ public class peakIndexInMountainArray {
 
     }
 }
+
