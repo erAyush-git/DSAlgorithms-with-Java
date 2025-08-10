131dc5bec76354f5e0ed73c19729a04d75e640e3
Ayush Tiwari <studentayush6@gmail.com>
2025-08-10 19:51:27 +0530

    Update findAllDuplicatesInArray.java

--- Competitive-Programming/Leetcode/sorting/findAllDuplicatesInArray.java
+++ Competitive-Programming/Leetcode/sorting/findAllDuplicatesInArray.java
@@ -1,3 +1,13 @@
+/*Given an integer array nums of length n where all the integers of 
+nums are in the range [1, n] and each integer appears at most twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant auxiliary space,
+excluding the space needed to store the output
+
+leetcode 442*/
+
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,3 +42,4 @@ public class findAllDuplicatesInArray {
         nums[s]=temp;
     }
 }
+
