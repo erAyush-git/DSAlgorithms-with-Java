d3d04c2bea29336b604c17a690c0a55891babc73
Ayushh <studentayush6@gmail.com>
2026-01-16 21:27:28 +0530

    WhileLoop_Revision

---
+++ revisionShit/RS_while.java
@@ -0,0 +1,11 @@
+package revisionShit;
+
+public class RS_while {
+    public static void main(String[] args){
+        int count = 0;
+        while(count <= 10){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
