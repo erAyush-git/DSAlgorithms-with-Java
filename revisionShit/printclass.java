11e701b8eeca21166967def73f191f98611f9c2b
Ayushh <studentayush6@gmail.com>
2026-01-16 22:09:47 +0530

    I guess I am DYING AGAIN !

---
+++ revisionShit/printclass.java
@@ -0,0 +1,12 @@
+package revisionShit;
+
+public class printclass {
+   public static void main(String[] args){
+    System.out.println("Aysuh");
+    System.out.print("Ayush 2 ");
+    String name = "Ayush ";
+    int num = 10;
+    System.out.printf("Name: %s , roll number = %d ", name , num);
+
+   } 
+}
