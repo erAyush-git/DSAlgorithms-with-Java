286af515c7d3f938e42c51e14fe16c477e4c6774
Ayushh <studentayush6@gmail.com>
2026-01-22 00:45:13 +0530

    arrayList folder added

---
+++ JavaCodes/arrays/arrayList/arrayListDecleration.java
@@ -0,0 +1,10 @@
+package JavaCodes.arrays.arrayList;
+
+import java.util.ArrayList;
+public class arrayListDecleration {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>(10);
+        list.add(50);
+        System.out.println(list);
+    }
+}
