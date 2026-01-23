5c1e93e5fd4bb1dc05aa0bc3faa599dc0ceab237
Ayushh <studentayush6@gmail.com>
2026-01-23 23:15:38 +0530

    added arr list

---
+++ JavaCodes/arrays/arrayList/arrayListString.java
@@ -0,0 +1,12 @@
+package JavaCodes.arrays.arrayList;
+
+import java.util.ArrayList;
+public class arrayListString {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(10);
+        list.add("Ayush");
+        list.add("Jiya");
+        System.out.println(list);
+        
+    }
+}
